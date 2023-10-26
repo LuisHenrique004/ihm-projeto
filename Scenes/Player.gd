5a333d4cbed85f16550036ea9d4f81278e71d33a
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-10-26 18:07:40 -0300

    Initial commit

---
+++ Scenes/Player.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+var grid_size = 16
+var inputs = {
+	'ui_up': Vector2.UP,
+	'ui_down': Vector2.DOWN,
+	'ui_left': Vector2.LEFT,
+	'ui_right': Vector2.RIGHT
+}
+
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+			
+func move(dir):
+	position += inputs[dir] * grid_size
+	
