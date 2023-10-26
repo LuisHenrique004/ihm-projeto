f7bcf7558d865b043bf3738f63429c21e3b44fb3
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-10-26 18:54:45 -0300

    Colisão do jogo

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+onready var ray = $RayCast2D
 var grid_size = 16
 var inputs = {
 	'ui_up': Vector2.UP,
@@ -14,5 +15,9 @@ func _unhandled_input(event):
 			move(dir)
 			
 func move(dir):
-	position += inputs[dir] * grid_size
+	var vector_pos = inputs[dir] * grid_size
+	ray.cast_to = vector_pos
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		position += vector_pos
 	
